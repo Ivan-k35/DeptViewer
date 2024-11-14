37feb6d0c4a69f729efe0ea94e9c7e63ea160431
Ivan Koshkin <ivan.koshkin35@gmail.com>
2024-11-14 17:37:52 +0300

    Update settings.py

--- dept_viewer/settings.py
+++ dept_viewer/settings.py
@@ -69,6 +69,7 @@ CACHES = {
         'LOCATION': env('REDIS_URL', default='redis://127.0.0.1:6379/1'),
         'OPTIONS': {
             'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+            'SERIALIZER': 'django_redis.serializers.json.JSONSerializer',
             'IGNORE_EXCEPTIONS': True,
         }
     }
