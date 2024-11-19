9d10b883b88a918c6eca972fa7fcad66021e40f3
Ivan Koshkin <ivan.koshkin35@gmail.com>
2024-11-19 17:57:59 +0300

    Update test_views.py in departments app

--- departments/tests/test_views.py
+++ departments/tests/test_views.py
@@ -1,14 +1,17 @@
 import pytest
 from django.urls import reverse
+from django.core.cache import cache
 from departments.models import Department
 
 
-@pytest.mark.django_db
+@pytest.mark.django_db(transaction=True)
 def test_department_tree_view(client):
     Department.objects.create(name="IT Department")
+    cache.delete('department_tree_data')
 
     url = reverse('departments:department_tree')
     response = client.get(url)
 
     assert response.status_code == 200
     assert "IT Department" in response.content.decode()
+    cache.delete('department_tree_data')
